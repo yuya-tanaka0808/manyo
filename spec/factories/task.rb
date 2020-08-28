f1e365a0cb86b95434dabac28039592ea758ebd7
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-08-28 18:27:29 +0900

    [add]:systemspecテスト３件記述,ブラウザ表示のためspec_helper.rbに記述

--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,7 +1,11 @@
 FactoryBot.define do
   factory :task do
     # 下記の内容は実際に作成するカラム名に合わせて変更してください
-    title { 'test_title' }
-    content { 'test_content' }
+    title { 'Factoryで作成したデフォルトタイトル１' }
+    content { 'Factoryで作成したデフォルトコンテント１' }
+  end
+  factory :second_task, class: Task do
+    title { 'Factoryで作ったデフォルトのタイトル２' }
+    content { 'Factoryで作ったデフォルトのコンテント２' }
   end
 end
