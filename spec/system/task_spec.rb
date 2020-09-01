3313fb95b6324517249d9a1dd56402561e9077a1
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-01 10:29:44 +0900

    [add]:systemspecテスト1件の追記

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+  let!(:task1) {FactoryBot.create(:task, title: 'task0',content: 'content0')}
   before do
     FactoryBot.create(:task)
     FactoryBot.create(:second_task)
@@ -23,7 +24,7 @@ RSpec.describe 'タスク管理機能', type: :system do
     context '一覧画面に遷移した場合' do
       it '作成済みのタスク一覧が表示される' do
         #テストで使用するためタスク作成
-        task = FactoryBot.create(:task, title: 'task', content: 'content')
+        task = FactoryBot.create(:task, title: 'task1', content: 'content1')
         visit tasks_path
         #タスク一覧ページに遷移出来ているか確認
         current_path
@@ -34,11 +35,19 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task'
       end
     end
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される'do
+        visit tasks_path
+        task_list = all('.task_row')
+        # タスク一覧を配列として取得するためにView側でidを降っておく
+        expect(task_list[0]).to have_content 'タイトル２'
+      end
+    end
   end
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
-         task = FactoryBot.create(:task, title: 'task', content:'content')
+         task = FactoryBot.create(:task, title: 'task2', content:'content2')
          visit task_path(task.id)
          expect(page).to have_content 'task'
        end
