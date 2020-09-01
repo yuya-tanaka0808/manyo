1d8b495a1167bbf7e49b9af4fd8056a2e56de1c1
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-01 22:03:51 +0900

    [fix]:i18nエラー文章の修正,[change]:Basic認証を本番環境のみに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :basic
-
+  before_action :basic, if: :production?
   private
+
+  def production?
+    Rails.env.production?
+  end
+
   def basic
     authenticate_or_request_with_http_basic do |name, password|
       name == ENV['BASIC_AUTH_NAME'] && password == ENV['BASIC_AUTH_PASSWORD']
