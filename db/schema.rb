202ecf450c55441cfddd1b1e511385cd0c2f5c17
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-03 11:49:29 +0900

    [change]:time_limitカラムにNULL制約を設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_27_115438) do
+ActiveRecord::Schema.define(version: 2020_09_03_023248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2020_08_27_115438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "priority"
-    t.datetime "time_limit"
+    t.datetime "time_limit", null: false
     t.string "status"
   end
 
