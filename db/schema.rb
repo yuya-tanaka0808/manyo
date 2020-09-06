8a7436b80fe87eb793f09fa03dc75cb7aebc0c40
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-06 18:04:13 +0900

    [add]:Userモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_05_034911) do
+ActiveRecord::Schema.define(version: 2020_09_06_081557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_09_05_034911) do
     t.datetime "time_limit", null: false
     t.string "status", null: false
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
