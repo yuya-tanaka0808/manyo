fafd51c53568edb28b311ed2405d8082fceb26e4
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-07 10:40:42 +0900

    [add]:sessionコントローラー,ヘルパーを記載

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+   before_action :find_user, only:[:show]
 
   def new
     @user = User.new
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render 'new'
@@ -14,12 +16,15 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
   end
 
 end
