1716922217ab38e8c76b291eaacc9ffad366cab2
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-07 13:22:23 +0900

    [add]:ログイン・ログアウト用リンク作成

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
    user = User.find_by(email: params[:session][:email].downcase)
    if user && user.authenticate(params[:session][:password])
      session[:user_id] = user.id
-     redirect_to pictures_path
+     redirect_to tasks_path
    else
      flash.now[:danger] = 'ログインに失敗しました'
      render :new
