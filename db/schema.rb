69a724bea6dff4209fdf64ec66fcfb56535c3b46
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-08 17:58:37 +0900

    [add]:userテーブルにadminカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_004334) do
+ActiveRecord::Schema.define(version: 2020_09_08_085632) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_004334) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
