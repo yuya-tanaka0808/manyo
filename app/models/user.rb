423677e292faacfa65932f167b0f24e8934d15a4
yuya tanaka <yuya.tanaka0808.w@gmail.com>
2020-09-08 21:57:57 +0900

    [change]:ユーザー削除で紐付いたタスク削除機能実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
 end
